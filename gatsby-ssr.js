ce6df7eca03f72f713238172be9e644fd586d41f
curlyjun <tjdwns4850@gmail.com>
2022-08-03 18:11:12 +0900

    feat: 다크모드 관련 로직 추가

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,9 +1,28 @@
+const React = require("react")
 /**
  * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
  *
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
 
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+exports.onRenderBody = ({ setHtmlAttributes, setPreBodyComponents }) => {
   setHtmlAttributes({ lang: `ko` })
+  setPreBodyComponents([
+    React.createElement("script", {
+      dangerouslySetInnerHTML: {
+        __html: `
+          let darkQuery = window.matchMedia('(prefers-color-scheme: dark)')
+          if (localStorage.theme === 'dark' || (!('theme' in localStorage) && darkQuery.matches)) {
+            document.documentElement.classList.add('dark')
+          } else {
+            document.documentElement.classList.remove('dark')
+          }
+
+          darkQuery.addEventListener('change', e => {
+            e.matches ? document.documentElement.classList.add('dark') : document.documentElement.classList.remove('dark')
+          })
+        `,
+      },
+    }),
+  ])
 }
