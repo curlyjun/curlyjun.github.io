26c1c4d4614167397a0bc4f42cf7efea8b682df6
curlyjun <tjdwns4850@gmail.com>
2022-08-08 23:40:16 +0900

    style: 스타일 업데이트

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            '.prose :where(code):not(:where([class~="not-prose"] *))': {
+              background: "red",
+              "&::after, &::before": {
+                content: "none",
+              },
+            },
+          },
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 }
